74942e6967e680950c6f327be2a624363b7a7001
okamurayasuyuki <haradashinya@gmail.com>
2014-02-15 20:40:28 +0900

    fix

--- basic_app/users/models.py
+++ basic_app/users/models.py
@@ -4,7 +4,6 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80), unique=True)
 
     def __init__(self, name):
         self.name = name
