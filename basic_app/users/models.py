a6b9cadedb7efd85253a55ff906fd03efe39361e
okamurayasuyuki <haradashinya@gmail.com>
2014-02-15 20:46:30 +0900

    add age

--- basic_app/users/models.py
+++ basic_app/users/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
+    age = db.Column(db.String(80), unique=True)
 
     def __init__(self, name):
         self.name = name
